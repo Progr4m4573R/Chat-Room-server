f5e67b6297f0e17f49e6f0a6ee49210f848bf777
Stephen <srerribekibele@gmail.com>
2023-03-16 00:25:36 +0000

    Fixed error with improper if and else statement usage in client.py

--- client.py
+++ client.py
@@ -12,24 +12,23 @@ Port = int(sys.argv[2])
 #connect to server
 server.connect((IP_address, Port))
 
-
 #change your username
 host = socket.gethostname()
 host_ip = socket.gethostbyname(host)
+
 while True:
     try:
-        if IP_address!=host_ip:
-            #receive welcome message from server  
-            message = server.recv(2048)
-            message = message.decode("utf-8") 
-            print(message)
-        else:
-            #send user message
-            message = bytes(input("<You> "),'utf-8')# fixed error with message not sending because it needed to be in bytes format
-            if 'Exit' == message:
-                break
-            #print(f'Processing Message from input() *****{message}*****')
-            server.send(message)
+        #receive welcome message from server  
+        message = server.recv(2048)
+        message = message.decode("utf-8") 
+        print(message)
+        
+        #send user message
+        message = bytes(input("<You> "),'utf-8')# fixed error with message not sending because it needed to be in bytes format
+        if 'Exit' == message:
+            break
+        #print(f'Processing Message from input() *****{message}*****')
+        server.send(message)
     except Exception as e:
         print("Error in client.py: ",e)
 server.close()
